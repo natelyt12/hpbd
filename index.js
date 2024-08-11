8f29748f028e253ac0ac854f9dc1e0b8f826c305
Natelyt <nathanelytra2k7@gmail.com>
2024-08-11 22:24:22 +0700

    tao muon yelan

---
+++ index.js
@@ -0,0 +1,7 @@
+// Popup
+let sitePopup = document.getElementsByClassName('site-popup')[0]
+let popupClBtn = document.getElementById('close-btn')
+
+popupClBtn.addEventListener('click', () => {
+    sitePopup.classList.remove('activated')
+})
