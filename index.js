21d8698a2aec46de64c424da0e2dd7efb52795b4
Natelyt <nathanelytra2k7@gmail.com>
2024-11-14 21:29:03 +0700

    update 11/14/2024

--- index.js
+++ index.js
@@ -7,35 +7,3 @@ popupClBtn.addEventListener('click', () => {
 })
 
 // Your code here
-let form = document.getElementById('form')
-let form1 = document.getElementById('a')
-
-form.addEventListener('submit', () => {
-    event.preventDefault()
-    let location = document.getElementById('location')
-    let sLocation = document.getElementById('submit-location')
-    savedAnim(location, sLocation)
-})
-
-a.addEventListener('submit', () => {
-    event.preventDefault()
-    let tab = document.getElementById('tab')
-    let stab = document.getElementById('submit-tab')
-    savedAnim(tab, stab)
-})
-
-function savedAnim(ev, btn) {
-    ev.style.setProperty('--wid', '100%')
-    btn.setAttribute('disabled', '')
-    setTimeout(() => {
-        ev.style.setProperty('--l', '1')
-        ev.style.setProperty('--wid', '0%')
-        ev.style.setProperty('--r', 'unset')
-    }, 500);
-    setTimeout(() => {
-        ev.style.setProperty('--l', '0')
-        ev.style.setProperty('--wid', '0%')
-        ev.style.setProperty('--r', 'unset')
-        btn.removeAttribute('disabled')
-    }, 1000);
-}
