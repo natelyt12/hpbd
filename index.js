678a72cad39b68a9cdd1e1f849668f3cb455d4ea
Natelyt <nathanelytra2k7@gmail.com>
2024-11-20 22:51:37 +0700

    a

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ popupClBtn.addEventListener('click', () => {
 })
 
 // Your code here
+loadcir = document.getElementById('loadcir')
+nav = document.querySelector('.navbar').classList
+logo = document.querySelector('.logo')
+
+loadtime = Math.random() * 3000
+setTimeout(() => {
+    loadcir.style.opacity = '0'
+    loadcir.style.height = '0px'
+    nav.add('done')
+    logo.style.scale = '0.3'
+    logo.style.margin = '0'
+}, loadtime);
